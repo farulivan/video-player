ca0c1c842cb8a0c2f7193f266b229b6d6efd4f55
Farul Ivan <farulivan@gmail.com>
2022-10-04 18:14:51 +0700

    finishing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Video } from './Video';
+import { Menu } from './Menu';
 import './App.css';
 
 function App() {
+
+  const VIDEOS = {
+    fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+    slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+    cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+    eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+  };
+
+  class VideoPlayer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { src: VIDEOS.fast };
+      this.chooseVideo = this.chooseVideo.bind(this)
+    }
+    
+    chooseVideo(newVideo) {
+      this.setState({ src: VIDEOS[newVideo] })
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Video Player</h1>
+          <Menu chooseVideo={this.chooseVideo}/>
+          <Video src={this.state.src}/>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VideoPlayer />
     </div>
   );
 }
